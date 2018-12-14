146b34478b226938e5a63d5c9ba482f57b6b318e
yx25cv <42892130+yx25cv@users.noreply.github.com>
2018-12-14 15:24:44 +0100

    Resen problem sa transakcijom

--- src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring5recipeapp.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
 
--- src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -2,11 +2,13 @@ package guru.springframework.spring5recipeapp.domain;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
 
