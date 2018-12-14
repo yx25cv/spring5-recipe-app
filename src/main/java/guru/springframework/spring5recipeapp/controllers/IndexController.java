325c62252f118332b4969f0b6fd371f42d738a62
yx25cv <42892130+yx25cv@users.noreply.github.com>
2018-12-14 15:12:24 +0100

    Commit sa implementiranim Lombok-om do kraja

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package guru.springframework.spring5recipeapp.controllers;
 
 import guru.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -25,6 +27,7 @@ public class IndexController {
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
 
+        log.debug("Getting index page");
         model.addAttribute("recipes", recipeService.getRecipes());
 
 /*        Optional<Category> categoryOptional = categoryRepository.findByCategoryName("American");
--- src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -1,10 +1,13 @@
 package guru.springframework.spring5recipeapp.domain;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
 @Entity
 public class UnitOfMeasure {
 
@@ -12,23 +15,4 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String uom;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUom() {
-        return uom;
-    }
-
-    public void setUom(String uom) {
-        this.uom = uom;
-    }
-
-
 }
